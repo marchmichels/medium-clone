463db378302dc99742169a9fa6e90d80b71346ab
marchmichels <56034198+marchmichels@users.noreply.github.com>
2023-05-03 15:40:52 -0400

    Implemented comments

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -3,13 +3,43 @@ import Header from "@/components/Header";
 import { sanityClient, urlFor } from "../../sanity";
 import { Post } from "@/typings";
 import PortableText from "react-portable-text";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useState } from "react";
+
+interface IFormInput {
+    _id: string;
+    name: string;
+    email: string;
+    comment: string;
+}
 
 interface Props {
     post: Post;
 }
 
 function Post({ post }: Props) {
-    console.log(post);
+
+    const [submitted, setSubmitted] = useState(false);
+
+
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit: SubmitHandler<IFormInput> = async (data) => {
+        await fetch('/api/createComment', {
+            method: 'POST',
+            body: JSON.stringify(data),
+        })
+            .then(() => {
+                console.log(data);
+                setSubmitted(true);
+            })
+            .catch((error) => {
+                console.log(error);
+                setSubmitted(false);
+            });
+    };
+
     return (
         <main>
             <Header />
@@ -51,7 +81,7 @@ function Post({ post }: Props) {
                                 li: ({ children }: any) => (
                                     <li className="ml-4 list-disc">{children}</li>
                                 ),
-                                link: ({href, children}: any) => (
+                                link: ({ href, children }: any) => (
                                     <a href={href} className="text-blue-500 hover:underline">
                                         {children}
                                     </a>
@@ -60,9 +90,89 @@ function Post({ post }: Props) {
                         }
                     />
                 </div>
-
-
             </article>
+
+            <hr className="max-w-lg my-5 mx-auto border border-yellow-500" />
+
+            {submitted ? (
+                <div className="flex flex-col p-10 my-10 bg-yellow-500 text-white max-w-2xl mx-auto">
+                    <h3 className="text-3xl font-bold">
+                        Thank you for submitting your comment!
+                    </h3>
+                    <p>Once it has been approved it will appear below!</p>
+                </div>
+
+            ) : (
+                <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col p-5 max-w-2xl mx-auto mb-10">
+                    <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+                    <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+                    <hr className="py-3 mt-2" />
+
+
+                    <input
+                        {...register("_id")}
+                        type="hidden"
+                        name="_id"
+                        value={post._id}
+                    />
+
+                    <label className="block mb-5">
+                        <span className="text-gray-700">Name</span>
+                        <input
+                            {...register("name", { required: true })}
+                            className="shadow border rounded py-2 px-3 form-input mt-1 block w-full outline-none ring-yellow-500 focus:ring"
+                            placeholder="John Appleseed"
+                            type="text" />
+                    </label>
+                    <label className="block mb-5">
+                        <span className="text-gray-700">Email</span>
+                        <input
+                            {...register("email", { required: true })}
+                            className="shadow border rounded py-2 px-3 form-input mt-1 block w-full outline-none ring-yellow-500 focus:ring"
+                            placeholder="John Appleseed"
+                            type="email" />
+                    </label>
+                    <label className="block mb-5">
+                        <span className="text-gray-700">Comment</span>
+                        <textarea
+                            {...register("comment", { required: true })}
+                            className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full outline-none ring-yellow-500 focus:ring"
+                            placeholder="John Appleseed"
+                            rows={8} />
+                    </label>
+
+                    {/* errors will return when field validation fails  */}
+                    <div className="flex flex-col p-5">
+                        {errors.name && (
+                            <span className="text-red-500">- The name field is required</span>
+                        )}
+                        {errors.comment && (
+                            <span className="text-red-500">- The comment field is required</span>
+                        )}
+                        {errors.email && (
+                            <span className="text-red-500">- The email field is required</span>
+                        )}
+                    </div>
+
+                    <input type="submit" className="bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer" />
+                </form>
+            )}
+
+            {/* Comments */}
+            <div className="flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2">
+                <h3 className="text-4xl">Comments</h3>
+                <hr className="pb-2"/>
+                {post.comments.map((comment) => (
+                    <div key={comment._id}>
+                        <p>
+                            <span className="text-yellow-500">{comment.name}: </span>{comment.comment}
+                        </p>
+                    </div>
+                ))}
+
+            </div>
+
+
         </main>
     )
 }
@@ -126,3 +236,4 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     };
 
 }
+
