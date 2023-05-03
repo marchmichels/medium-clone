4efcd3ab821e2881d53631a11c43a25276661d19
marchmichels <56034198+marchmichels@users.noreply.github.com>
2023-05-03 15:47:03 -0400

    Fixed ESLint error

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home({ posts }: Props) {
         <div className="px-10 space-y-5">
           <h1 className="text-6xl max-w-xl font-serif"><span className="underline decoration-black decoration-4">Medium</span> is a place to write, read, and connect</h1>
           <h2>
-            It's easy and free to post your thinking on any topic and connect with millions of readers.
+            It&apos;s easy and free to post your thinking on any topic and connect with millions of readers.
           </h2>
         </div>
 
